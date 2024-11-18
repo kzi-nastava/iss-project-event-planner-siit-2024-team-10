c4b957af7a343c14e0e9a7ab9517fae74007e3f0
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-18 11:29:35 +0100

    Add event model

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Event.java
@@ -0,0 +1,19 @@
+package com.ftn.iss.eventPlanner.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Event {
+    private int id;
+    private Organizer organizer;
+    private EventType eventType;
+    private String name;
+    private String description;
+    private int maxParticipants;
+    private boolean isOpen;
+    private LocalDate date;
+    private List<String> guestList;
+    private boolean isDeleted;
+    private Location location;
+    private List<AgendaItem> agenda;
+}
