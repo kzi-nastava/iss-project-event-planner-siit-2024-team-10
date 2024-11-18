9b961e3e2bcbab4e42008f0dcd6f8dfd0b97ee8e
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-18 11:29:50 +0100

    Add offering model

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Offering.java
@@ -0,0 +1,8 @@
+package com.ftn.iss.eventPlanner.model;
+
+public abstract class Offering {
+    private int id;
+    private boolean pending;
+    private boolean isDeleted;
+    private Provider provider;
+}
---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/OfferingCategory.java
@@ -0,0 +1,9 @@
+package com.ftn.iss.eventPlanner.model;
+
+public class OfferingCategory {
+    private int id;
+    private String name;
+    private String description;
+    private boolean isDeleted;
+    private boolean pending;
+}
