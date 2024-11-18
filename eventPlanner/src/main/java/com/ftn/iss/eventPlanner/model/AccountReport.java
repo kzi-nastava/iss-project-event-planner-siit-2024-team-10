5544db6be759bc94007f75e6908c056f1988d0ab
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-18 14:25:34 +0100

    Add missing ids

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/AccountReport.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/AccountReport.java
@@ -3,6 +3,7 @@ package com.ftn.iss.eventPlanner.model;
 import java.time.LocalDateTime;
 
 public class AccountReport {
+    private int id;
     private String description;
     private LocalDateTime processingTimestamp;
     private Status status;
--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Location.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Location.java
@@ -1,6 +1,7 @@
 package com.ftn.iss.eventPlanner.model;
 
 public class Location {
+    private int id;
     private String city;
     private String country;
     private String street;
