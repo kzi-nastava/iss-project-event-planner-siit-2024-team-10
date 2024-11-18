56510a09034a474a26ce155ac67ee9fa764f9201
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-18 13:51:39 +0100

    Add service model

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Service.java
@@ -0,0 +1,8 @@
+package com.ftn.iss.eventPlanner.model;
+
+import java.util.List;
+
+public class Service extends Offering{
+    private ServiceDetails currentDetails;
+    private List<ServiceDetails> detailsHistory;
+}
