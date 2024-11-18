5ba01f44d286ec752e09267214011a15d311d92f
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-18 13:51:33 +0100

    Add service details model

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/ServiceDetails.java
@@ -0,0 +1,23 @@
+package com.ftn.iss.eventPlanner.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ServiceDetails {
+    private int id;
+    private String name;
+    private String description;
+    private String specification;
+    private double price;
+    private double discount;
+    private List<String> photos;
+    private int maxDuration;
+    private int minDuration;
+    private int cancellationPeriod;
+    private int reservationPeriod;
+    private boolean isVisible;
+    private boolean isAvailable;
+    private boolean autoConfirm;
+    private LocalDateTime timestamp;
+    private List<EventType> eventTypes;
+}
