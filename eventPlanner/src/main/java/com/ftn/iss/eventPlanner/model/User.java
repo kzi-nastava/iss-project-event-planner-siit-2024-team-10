2665d46de6fd744bb0cd71a8da153656fd2d494d
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-21 09:57:02 +0100

    Add getters and setters to user model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/User.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/User.java
@@ -1,5 +1,10 @@
 package com.ftn.iss.eventPlanner.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class User {
     private int id;
     private String firstName;
