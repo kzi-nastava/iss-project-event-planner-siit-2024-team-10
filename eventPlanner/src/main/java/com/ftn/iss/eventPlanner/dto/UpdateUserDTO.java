e70e80da395fa114dc287fe196eaa98e00a29a58
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-22 18:08:42 +0100

    Add update user dto

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/UpdateUserDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/UpdateUserDTO.java
@@ -1,9 +1,18 @@
 package com.ftn.iss.eventPlanner.dto;
 
+import com.ftn.iss.eventPlanner.model.Role;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateUserDTO {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String profilePhoto;
+    private LocationDTO location;
+    private UpdateCompanyDTO company;
+
+    public UpdateUserDTO() {}
 }
