daa9d8854844c17efdec1a87a4dc93d6290ffcaa
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-22 09:35:28 +0100

    Add get product dto

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/GetProductDTO.java
@@ -0,0 +1,23 @@
+package com.ftn.iss.eventPlanner.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GetProductDTO {
+    private int categoryId;
+    private boolean pending;
+    private int providerID;
+    private String name;
+    private String description;
+    private double price;
+    private double discount;
+    private List<String> photos;
+    private boolean isVisible;
+    private boolean isAvailable;
+
+    public GetProductDTO() {}
+}
