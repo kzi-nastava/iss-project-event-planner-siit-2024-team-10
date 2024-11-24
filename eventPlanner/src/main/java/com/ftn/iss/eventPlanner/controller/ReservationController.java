6cb617bd471c269736f6eade304569e05ebb1800
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-24 21:07:42 +0100

    Add create reservation endpoint

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/ReservationController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/ReservationController.java
@@ -1,4 +1,28 @@
 package com.ftn.iss.eventPlanner.controller;
 
+import com.ftn.iss.eventPlanner.dto.CreateReservationDTO;
+import com.ftn.iss.eventPlanner.dto.CreatedReservationDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservations")
 public class ReservationController {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CreatedReservationDTO> createProduct(@RequestBody CreateReservationDTO reservation) throws Exception {
+        CreatedReservationDTO createdReservation = new CreatedReservationDTO();
+        createdReservation.setId(1);
+        createdReservation.setStatus(reservation.getStatus());
+        createdReservation.setStartTime(reservation.getStartTime());
+        createdReservation.setEndTime(reservation.getEndTime());
+        createdReservation.setServiceId(reservation.getServiceId());
+        createdReservation.setServiceId(reservation.getServiceId());
+
+        return new ResponseEntity<CreatedReservationDTO>(createdReservation, HttpStatus.CREATED);
+    }
 }
