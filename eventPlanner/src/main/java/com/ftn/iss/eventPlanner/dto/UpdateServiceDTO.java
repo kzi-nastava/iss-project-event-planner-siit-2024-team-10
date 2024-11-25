2d81077d652e2409c58365393baa99c66d96f4b7
milicaradicc <milica.t.radic@gmail.com>
2024-11-25 20:27:32 +0100

    Add update service dto

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/UpdateServiceDTO.java
@@ -0,0 +1,23 @@
+package com.ftn.iss.eventPlanner.dto;
+
+import java.util.List;
+
+public class UpdateServiceDTO {
+	private String name;
+    private String description;
+    private String specification;
+    private double price;
+    private double discount;
+    private List<String> photos;
+    private boolean isVisible;
+    private boolean isAvailable;
+    private int maxDuration;
+    private int minDuration;
+    private int cancellationPeriod;
+    private int reservationPeriod;
+    private boolean autoConfirm;
+    
+    public UpdateServiceDTO() {
+    	
+    }
+}
