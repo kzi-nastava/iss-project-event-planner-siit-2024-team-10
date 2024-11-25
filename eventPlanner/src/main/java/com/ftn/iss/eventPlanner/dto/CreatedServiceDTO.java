438b294a169e14689f008dab4ce0105ba17b1f9c
milicaradicc <milica.t.radic@gmail.com>
2024-11-25 20:26:48 +0100

    Add created service dto

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/CreatedServiceDTO.java
@@ -0,0 +1,33 @@
+package com.ftn.iss.eventPlanner.dto;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreatedServiceDTO {
+    private int categoryId;
+    private String categoryProposal;
+    private boolean pending;
+    private int providerID;
+    private String name;
+    private String description;
+    private String specification;
+    private double price;
+    private double discount;
+    private List<String> photos;
+    private boolean isVisible;
+    private boolean isAvailable;
+    private int maxDuration;
+    private int minDuration;
+    private int cancellationPeriod;
+    private int reservationPeriod;
+    private boolean autoConfirm;
+
+    private List<String> comments;
+    private double averageRating; 
+
+    public CreatedServiceDTO() {
+    }
+}
