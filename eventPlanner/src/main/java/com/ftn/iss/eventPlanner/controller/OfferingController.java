ff3fc1a9b980a9624e0dcace3f2f2ff77db6653f
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-25 18:37:29 +0100

    Add filter params to getOfferings endpoint

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/OfferingController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/OfferingController.java
@@ -1,14 +1,16 @@
 package com.ftn.iss.eventPlanner.controller;
 
 import com.ftn.iss.eventPlanner.dto.GetOfferingDTO;
-import com.ftn.iss.eventPlanner.dto.GetProductDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Pageable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -17,7 +19,22 @@ import java.util.Collection;
 @RequestMapping("/api/offerings")
 public class OfferingController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Collection<GetOfferingDTO>> getOfferings(){
+    public ResponseEntity<Collection<GetOfferingDTO>> getOfferings(
+            @RequestParam(required = false) boolean isServiceFilter,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer eventTypeId,
+            @RequestParam(required = false) Integer categoryId,
+            @RequestParam(required = false) String location,
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) Integer minDiscount,
+            @RequestParam(required = false) Integer duration,
+            @RequestParam(required = false) Double minRating,
+            @RequestParam(required = false) LocalDate startDate,
+            @RequestParam(required = false) LocalDate endDate,
+            @RequestParam(required = false) boolean isAvailable,
+            Pageable pageable
+    ){
         Collection<GetOfferingDTO> offerings = new ArrayList<>();
 
         GetOfferingDTO offering1 = new GetOfferingDTO();
@@ -43,19 +60,43 @@ public class OfferingController {
         offering2.setProviderId(5);
 
         GetOfferingDTO offering3 = new GetOfferingDTO();
-        offering3.setId(1);
-        offering3.setName("Test Offering 1");
-        offering3.setCategoryId(2);
-        offering3.setDescription("Description 1");
-        offering3.setPrice(123);
+        offering3.setId(3);
+        offering3.setName("Test Offering 3");
+        offering3.setCategoryId(3);
+        offering3.setDescription("Description 3");
+        offering3.setPrice(1223);
         offering3.setPhotos(Arrays.asList("img.png","img2.png"));
         offering3.setDiscount(20);
-        offering3.setRating(2.4);
+        offering3.setRating(2.44);
         offering3.setProviderId(3);
 
+        GetOfferingDTO offering4 = new GetOfferingDTO();
+        offering4.setId(4);
+        offering4.setName("Test Offering 4");
+        offering4.setCategoryId(2);
+        offering4.setDescription("Description 4");
+        offering4.setPrice(123);
+        offering4.setPhotos(Arrays.asList("img4.png","img5.png"));
+        offering4.setDiscount(20);
+        offering4.setRating(2.4);
+        offering4.setProviderId(5);
+
+        GetOfferingDTO offering5 = new GetOfferingDTO();
+        offering5.setId(5);
+        offering5.setName("Test Offering 5");
+        offering5.setCategoryId(5);
+        offering5.setDescription("Description 5");
+        offering5.setPrice(123);
+        offering5.setPhotos(Arrays.asList("img.png","img2.png"));
+        offering5.setDiscount(20);
+        offering5.setRating(2.4);
+        offering5.setProviderId(3);
+
         offerings.add(offering1);
-        offerings.add(offering2);
-        offerings.add(offering3);
+        offerings.add(offering4);
+        offerings.add(offering5);
+        offerings.add(offering4);
+        offerings.add(offering5);
 
         return new ResponseEntity<Collection<GetOfferingDTO>>(offerings, HttpStatus.OK);
         }
