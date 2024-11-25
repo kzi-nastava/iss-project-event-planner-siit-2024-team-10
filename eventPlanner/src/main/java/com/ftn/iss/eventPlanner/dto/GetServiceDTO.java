c3abbce020b18b241f843aa236a8370f25f4b20a
milicaradicc <milica.t.radic@gmail.com>
2024-11-25 20:27:10 +0100

    Add get service dto

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/GetServiceDTO.java
@@ -0,0 +1,30 @@
+package com.ftn.iss.eventPlanner.dto;
+
+import java.util.List;
+
+public class GetServiceDTO {
+    private int categoryId;
+    private String categoryProposal;
+    private boolean pending;
+    private int providerID;
+    private String name;
+    private String description;
+    private String specification;
+    private double price;
+    private double discount;
+    private List<String> photos;
+    private boolean isVisible;
+    private boolean isAvailable;
+    private int maxDuration;
+    private int minDuration;
+    private int cancellationPeriod;
+    private int reservationPeriod;
+    private boolean autoConfirm;
+
+    private List<String> comments;
+    private double averageRating; 
+
+    public GetServiceDTO() {
+    	
+    }
+}
