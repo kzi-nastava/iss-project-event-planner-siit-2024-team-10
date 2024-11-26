05ce7a2cc322521c22454344db07af90724ff4c2
Mijat Krivokapić <mijat2004@gmail.com>
2024-11-26 12:06:44 +0100

    Change content in paged response from list to collection

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/PagedResponse.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/PagedResponse.java
@@ -3,16 +3,16 @@ package com.ftn.iss.eventPlanner.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.Collection;
 
 @Getter
 @Setter
 public class PagedResponse<T>{
-    private List<T> content;
+    private Collection<T> content;
     private int totalPages;
     private long totalElements;
 
-    public PagedResponse(List<T> content, int totalPages, long totalElements) {
+    public PagedResponse(Collection<T> content, int totalPages, long totalElements) {
         this.content = content;
         this.totalPages = totalPages;
         this.totalElements = totalElements;
