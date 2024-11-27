fceaa34db955aa3d7ff2c7e448a7146e0a9fb2e1
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-27 14:34:16 +0100

    Add get reservation DTO

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/GetReservationDTO.java
@@ -0,0 +1,21 @@
+package com.ftn.iss.eventPlanner.dto;
+
+import com.ftn.iss.eventPlanner.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class GetReservationDTO {
+    private int id;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Status status;
+    private int eventId;
+    private int serviceId;
+
+    public GetReservationDTO() {
+    }
+}
