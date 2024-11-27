d99e6c814385e4dc13be960692f2654f4bd727bd
milicaradicc <milica.t.radic@gmail.com>
2024-11-27 12:10:32 +0100

    Add created rating dto

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/CreatedRatingDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/CreatedRatingDTO.java
@@ -1,2 +1,11 @@
-package com.ftn.iss.eventPlanner.dto;public class CreatedRatingDTO {
+package com.ftn.iss.eventPlanner.dto;
+
+public class CreatedRatingDTO {
+    private int id;
+    private int score;
+    private int accoundId;
+    public CreatedRatingDTO(){
+        
+    }
+
 }
