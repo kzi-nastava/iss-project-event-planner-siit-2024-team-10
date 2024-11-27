a202a80c92de3d85bae917563acfba45a8379bb0
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-11-27 13:30:54 +0100

    Remove unused imports

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/EventController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/EventController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.filter.RequestContextFilter;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
