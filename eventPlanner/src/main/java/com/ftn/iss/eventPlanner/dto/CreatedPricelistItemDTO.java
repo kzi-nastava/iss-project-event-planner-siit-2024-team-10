cdc39441a194b10c2c159bd2cc69c274dc73393f
milicaradicc <milica.t.radic@gmail.com>
2024-11-27 17:01:47 +0100

    Add created price list item dto

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/CreatedPricelistItemDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/CreatedPricelistItemDTO.java
@@ -1,2 +1,16 @@
-package com.ftn.iss.eventPlanner.dto;public class CreatedPricelistItemDTO {
+package com.ftn.iss.eventPlanner.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreatedPricelistItemDTO {
+    private int id;
+    private String name;
+    private double price;
+    private double discount;
+
+    public CreatedPricelistItemDTO() {
+    }
 }
