4508ac6c1e29565c9b0b2f285ce06712ea61a79a
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-03 17:13:48 +0100

    Add JPA annotations to Account model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Account.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Account.java
@@ -1,28 +1,49 @@
 package com.ftn.iss.eventPlanner.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
+    @Enumerated
     private Role role;
+    @Column(nullable = false)
     private LocalDateTime registrationTimestamp;
+    @Column(nullable = false)
     private boolean notificationsSilenced;
+    @Column(nullable = false)
+    @Enumerated
     private AccountStatus status;
-    private List<Event> favouriteEvents;
-    private List<Event> acceptedEvents;
+    @ManyToMany
+    private Set<Event> favouriteEvents = new HashSet<>();
+    @ManyToMany
+    private Set<Event> acceptedEvents = new HashSet<>();;
+    @OneToOne
     private User user;
-    private List<Offering> favouriteOfferings;
-    private List<Notification> notifications;
-    private List<Account> blockedAccounts;
+    @ManyToMany
+    private Set<Offering> favouriteOfferings = new HashSet<>();;
+    @OneToMany
+    private Set<Notification> notifications = new HashSet<>();;
+    @ManyToMany
+    private Set<Account> blockedAccounts = new HashSet<>();;
 
     public Account() {
     }
