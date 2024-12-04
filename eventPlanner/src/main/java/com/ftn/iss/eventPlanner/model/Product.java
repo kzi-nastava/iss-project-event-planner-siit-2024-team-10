4e22fb4beb484bd10d19f1269bec9a199f59ff9f
milicaradicc <milica.t.radic@gmail.com>
2024-12-04 14:17:03 +0100

    Add JPA annotations to ServiceDetails model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Product.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Product.java
@@ -1,15 +1,25 @@
 package com.ftn.iss.eventPlanner.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
+@Entity
 public class Product extends Offering {
+    @Column
+    @OneToOne
     private ProductDetails currentDetails;
-    private List<ProductDetails> detailsHistory;
+    @Column
+    @OneToMany
+    private Set<ProductDetails> detailsHistory = new HashSet<>();;
 
     public Product() {
     }
