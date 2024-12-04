d596d3fb238d166812f5fb8705101e313e55290e
milicaradicc <milica.t.radic@gmail.com>
2024-12-04 13:38:04 +0100

    Add JPA annotations to Comment model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Comment.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Comment.java
@@ -2,13 +2,22 @@ package com.ftn.iss.eventPlanner.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import jakarta.persistence.*;
 @Getter
 @Setter
+@AllArgsConstructor
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String content;
+    @Column(nullable = false)
+    @Enumerated
     private Status status;
+    @Column(nullable = false)
+    @ManyToOne
     private Account commenter;
 
     public Comment() {
