2f0723d9c8f23dd97359469a3d2e7b4bf35bdf69
milicaradicc <milica.t.radic@gmail.com>
2024-12-04 13:42:47 +0100

    Add JPA annotations to Message model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Message.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Message.java
@@ -2,17 +2,29 @@ package com.ftn.iss.eventPlanner.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String content;
+    @Column(nullable = false)
     private LocalDateTime timestamp;
+    @Column(nullable = false)
+    @ManyToOne
     private Account sender;
+    @Column(nullable = false)
+    @ManyToOne
     private Account receiver;
+    @Column(nullable = false)
     private boolean isRead;
 
     public Message() {
