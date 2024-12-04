956e4be61baa5a74c909248908384f61a7f01f82
milicaradicc <milica.t.radic@gmail.com>
2024-12-04 14:23:29 +0100

    Add JPA annotations to Rating model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Rating.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Rating.java
@@ -1,13 +1,23 @@
 package com.ftn.iss.eventPlanner.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@Entity
 public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
+
     private int score;
+    @Column(nullable = false)
+    @ManyToOne
     private Account rater;
 
     public Rating() {
