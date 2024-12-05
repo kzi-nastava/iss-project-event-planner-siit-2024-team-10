dbf1c1ae7a403540abf2f201ec2cbc748ffb08ab
milicaradicc <milica.t.radic@gmail.com>
2024-12-05 08:46:00 +0100

    Update repositories

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/MessageRepository.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/MessageRepository.java
@@ -1,7 +1,7 @@
 package com.ftn.iss.eventPlanner.repositories;
 
-import com.ftn.iss.eventPlanner.model.Location;
+import com.ftn.iss.eventPlanner.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageRepository extends JpaRepository<Location,Integer> {
+public interface MessageRepository extends JpaRepository<Message,Integer> {
 }
