52af215edeb78970444e7e69b1e5d9192c6d90d3
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-06 23:40:33 +0100

    Add validation to create location dto

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/location/CreateLocationDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/location/CreateLocationDTO.java
@@ -1,14 +1,29 @@
 package com.ftn.iss.eventPlanner.dto.location;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateLocationDTO {
+    @NotNull(message = "City cannot be null")
+    @NotBlank(message = "City cannot be blank")
     private String city;
+
+    @NotNull(message = "Country cannot be null")
+    @NotBlank(message = "Country cannot be blank")
     private String country;
+
+    @NotNull(message = "Street cannot be null")
+    @NotBlank(message = "Street cannot be blank")
     private String street;
+
+    @NotNull(message = "House number cannot be null")
+    @NotBlank(message = "House number cannot be blank")
+    @Pattern(regexp = "\\d+", message = "House number must be a numeric string")
     private String houseNumber;
 
     public CreateLocationDTO() {}
