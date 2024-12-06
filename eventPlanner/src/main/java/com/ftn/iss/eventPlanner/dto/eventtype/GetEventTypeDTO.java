a1abc6fbb485b20ed36c9197af9c8f0e431e3d13
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-06 11:00:52 +0100

    Add get event type dto

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/eventtype/GetEventTypeDTO.java
@@ -0,0 +1,18 @@
+package com.ftn.iss.eventPlanner.dto.eventtype;
+
+import com.ftn.iss.eventPlanner.dto.GetOfferingDTO;
+import com.ftn.iss.eventPlanner.dto.offeringcategory.GetOfferingCategoryDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GetEventTypeDTO {
+    private int id;
+    private String name;
+    private String description;
+    private boolean isActive;
+    private List<GetOfferingCategoryDTO> recommendedCategories;
+}
