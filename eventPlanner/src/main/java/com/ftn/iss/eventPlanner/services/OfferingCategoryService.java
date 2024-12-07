ba20a7568273589ed56ea82a64070c99c13f99db
milicaradicc <milica.t.radic@gmail.com>
2024-12-07 12:27:01 +0100

    Add OfferingCategoryService

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/services/OfferingCategoryService.java
@@ -0,0 +1,27 @@
+package com.ftn.iss.eventPlanner.services;
+
+import com.ftn.iss.eventPlanner.dto.eventtype.GetEventTypeDTO;
+import com.ftn.iss.eventPlanner.dto.offeringcategory.GetOfferingCategoryDTO;
+import com.ftn.iss.eventPlanner.model.EventType;
+import com.ftn.iss.eventPlanner.model.OfferingCategory;
+import com.ftn.iss.eventPlanner.repositories.OfferingCategoryRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+
+public class OfferingCategoryService {
+    @Autowired
+    private OfferingCategoryRepository offeringCategoryRepository;
+    private ModelMapper modelMapper = new ModelMapper();
+
+    public List<GetOfferingCategoryDTO> findAll(){
+        List<OfferingCategory> offeringCategorys = offeringCategoryRepository.findAll();
+        return offeringCategorys.stream()
+                .map(offeringCategory -> modelMapper.map(offeringCategory, GetOfferingCategoryDTO.class))
+                .collect(Collectors.toList());
+    }
+}
