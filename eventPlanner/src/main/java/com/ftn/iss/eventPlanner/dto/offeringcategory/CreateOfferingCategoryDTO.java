476423ef15997a2b60a82c8b42390c59b7579e6c
milicaradicc <milica.t.radic@gmail.com>
2024-12-07 23:27:54 +0100

    Add CreateOfferingCategoryDTO

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/offeringcategory/CreateOfferingCategoryDTO.java
@@ -0,0 +1,14 @@
+package com.ftn.iss.eventPlanner.dto.offeringcategory;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateOfferingCategoryDTO {
+    private String name;
+    private String description;
+    public CreateOfferingCategoryDTO(){
+
+    }
+}
