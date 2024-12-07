efc7632f03ca96c2d6a90abc2a449bde841e7bbd
milicaradicc <milica.t.radic@gmail.com>
2024-12-07 23:28:34 +0100

    Add UpdatedOfferingCategoryDTO

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/offeringcategory/UpdatedOfferingCategoryDTO.java
@@ -0,0 +1,14 @@
+package com.ftn.iss.eventPlanner.dto.offeringcategory;
+
+import com.ftn.iss.eventPlanner.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdatedOfferingCategoryDTO {
+    private int id;
+    private String name;
+    private Status description;
+    public UpdatedOfferingCategoryDTO(){}
+}
