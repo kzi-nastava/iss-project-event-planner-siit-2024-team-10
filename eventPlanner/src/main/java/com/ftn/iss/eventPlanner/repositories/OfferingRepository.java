397e437abd7182e8486c6c717432c6cc7be02678
milicaradicc <milica.t.radic@gmail.com>
2024-12-07 13:06:28 +0100

    Add findAllServices

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/OfferingRepository.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/OfferingRepository.java
@@ -2,7 +2,13 @@ package com.ftn.iss.eventPlanner.repositories;
 
 import com.ftn.iss.eventPlanner.model.Location;
 import com.ftn.iss.eventPlanner.model.Offering;
+import com.ftn.iss.eventPlanner.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OfferingRepository extends JpaRepository<Offering,Integer> {
+    @Query("SELECT s FROM Service s")
+    List<Service> findAllServices();
 }
