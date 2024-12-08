b85a542e2ba3f0ac99249a2b3cdde9813c2e445f
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2024-12-08 19:59:49 +0100

    Remove unused imports

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/EventRepository.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/EventRepository.java
@@ -1,11 +1,8 @@
 package com.ftn.iss.eventPlanner.repositories;
 
 import com.ftn.iss.eventPlanner.model.Event;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.awt.print.Pageable;
 
 public interface EventRepository extends JpaRepository<Event,Integer>, JpaSpecificationExecutor<Event> {
 }
