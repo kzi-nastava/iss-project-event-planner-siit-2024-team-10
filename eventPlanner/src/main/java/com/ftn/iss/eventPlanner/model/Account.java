5cef35f837333b638730ec7c43f258d16ec57adc
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-15 13:34:35 +0100

    Add user details implementation to Account model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Account.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Account.java
@@ -4,17 +4,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @Entity
-public class Account {
+public class Account implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -29,6 +31,8 @@ public class Account {
     private LocalDateTime registrationTimestamp;
     @Column(nullable = false)
     private boolean notificationsSilenced;
+    @Column(nullable = false)
+    private Timestamp lastPasswordResetDate;
     @Column(nullable = false)
     @Enumerated
     private AccountStatus status;
@@ -46,5 +50,42 @@ public class Account {
     private Set<Account> blockedAccounts = new HashSet<>();;
 
     public Account() {
+    }
+
+    public void setPassword(String password) {
+        Timestamp now = new Timestamp(new Date().getTime());
+        this.setLastPasswordResetDate(now);
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return status!=AccountStatus.INACTIVE;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return status!=AccountStatus.SUSPENDED;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return status == AccountStatus.ACTIVE;
     }
 }
