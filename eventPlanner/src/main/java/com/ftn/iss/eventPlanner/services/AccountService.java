43c8eba647324cedf526d5e19dee2455444db73a
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-15 13:49:11 +0100

    Implement Account service

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/services/AccountService.java
@@ -0,0 +1,33 @@
+package com.ftn.iss.eventPlanner.services;
+
+import com.ftn.iss.eventPlanner.dto.authentication.RegisterDTO;
+import com.ftn.iss.eventPlanner.model.Account;
+import com.ftn.iss.eventPlanner.model.AccountStatus;
+import com.ftn.iss.eventPlanner.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AccountService implements UserDetailsService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(username);
+        if (account == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return account;
+        }
+    }
+}
