67aa0b0dede23c0bb9e1f23b854a504826daee2f
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-15 13:35:14 +0100

    Add findByEmail method to account repository

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/AccountRepository.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/AccountRepository.java
@@ -4,4 +4,5 @@ import com.ftn.iss.eventPlanner.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account,Integer> {
+    Account findByEmail(String email);
 }
