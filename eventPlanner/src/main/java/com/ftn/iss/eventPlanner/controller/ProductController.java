70d2b9dcaf0f76504322b20335f0de3902b20a32
milicaradicc <milica.t.radic@gmail.com>
2024-12-17 10:45:47 +0100

    Remove mock data

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/ProductController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/ProductController.java
@@ -2,18 +2,15 @@ package com.ftn.iss.eventPlanner.controller;
 
 import com.ftn.iss.eventPlanner.dto.*;
 import com.ftn.iss.eventPlanner.dto.product.*;
-import com.ftn.iss.eventPlanner.dto.service.GetServiceDTO;
 import com.ftn.iss.eventPlanner.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 @CrossOrigin
@@ -41,7 +38,7 @@ public class ProductController {
 
     @GetMapping( produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PagedResponse<GetProductDTO>> getProductsPage(
-            SpringDataWebProperties.Pageable page,
+            Pageable page,
             @RequestParam(required = false) Integer providerId,
             @RequestParam(required = false) Integer categoryId,
             @RequestParam(required = false) Integer eventTypeId,
@@ -53,25 +50,13 @@ public class ProductController {
             @RequestParam(required = false) Boolean isAvailable,
             @RequestParam(required = false) String name
     ) {
-        PagedResponse<GetProductDTO> response = productService.findAll(page, name, categoryId, eventTypeId, minPrice, maxPrice, isAvailable);
+        PagedResponse<GetProductDTO> response = productService.findAll(page,name,eventTypeId,categoryId,minPrice,maxPrice,isAvailable);
         return new ResponseEntity<PagedResponse<GetProductDTO>>(response, HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GetProductDTO> getProduct(@PathVariable("id") int id, @RequestParam(required = false) LocalDateTime historyTimestamp) {
         GetProductDTO product = new GetProductDTO();
-        product.setId(id);
-        product.setCategoryId(1);
-        product.setPending(false);
-        product.setProviderID(104);
-        product.setName("Luxury Event Decoration");
-        product.setDescription("Top-tier decorations for upscale events.");
-        product.setPrice(2500.00);
-        product.setDiscount(0.0);
-        product.setPhotos(Arrays.asList("https://example.com/photos/decor1.jpg"));
-        product.setVisible(true);
-        product.setAvailable(false);
-
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
@@ -79,33 +64,12 @@ public class ProductController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CreatedProductDTO> createProduct(@RequestBody CreateProductDTO product) throws Exception {
         CreatedProductDTO createdProduct = new CreatedProductDTO();
-        createdProduct.setId(1);
-        createdProduct.setCategoryId(product.getCategoryId());
-        createdProduct.setPending(product.isPending());
-        createdProduct.setProviderID(product.getProviderID());
-        createdProduct.setName(product.getName());
-        createdProduct.setDescription(product.getDescription());
-        createdProduct.setPrice(product.getPrice());
-        createdProduct.setDiscount(product.getDiscount());
-        createdProduct.setPhotos(product.getPhotos());
-        createdProduct.setVisible(product.isVisible());
-        createdProduct.setAvailable(product.isAvailable());
-
         return new ResponseEntity<CreatedProductDTO>(createdProduct, HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UpdatedProductDTO> updateProduct(@RequestBody UpdateProductDTO product, @PathVariable("id") int id) {
         UpdatedProductDTO updatedProduct = new UpdatedProductDTO();
-        updatedProduct.setId(1);
-        updatedProduct.setName(product.getName());
-        updatedProduct.setDescription(product.getDescription());
-        updatedProduct.setPrice(product.getPrice());
-        updatedProduct.setDiscount(product.getDiscount());
-        updatedProduct.setPhotos(product.getPhotos());
-        updatedProduct.setVisible(product.isVisible());
-        updatedProduct.setAvailable(product.isAvailable());
-
         return new ResponseEntity<UpdatedProductDTO>(updatedProduct, HttpStatus.OK);
     }
 
