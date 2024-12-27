b626ae860260ad4b37994a54987f6a65816fa41c
Mijat Krivokapić <mijat2004@gmail.com>
2024-12-27 16:55:52 +0100

    Add create agenda item dtos

---
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/agendaitem/CreatedAgendaItemDTO.java
@@ -0,0 +1,16 @@
+package com.ftn.iss.eventPlanner.dto.agendaitem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Getter
+@Setter
+public class CreatedAgendaItemDTO {
+    private String name;
+    private String description;
+    private String location;
+    private LocalTime startTime;
+    private LocalTime endTime;
+}
