f2185a8827571d3d7bc251d7c3ae1dd6cb47d914
milicaradicc <milica.t.radic@gmail.com>
2025-01-03 18:12:37 +0100

    Update controller to use service

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/MessageController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/MessageController.java
@@ -1,7 +1,9 @@
 package com.ftn.iss.eventPlanner.controller;
-import com.ftn.iss.eventPlanner.model.Message;
-import com.ftn.iss.eventPlanner.repositories.MessageRepository;
+import com.ftn.iss.eventPlanner.dto.message.GetMessageDTO;
+import com.ftn.iss.eventPlanner.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,11 @@ import java.util.List;
 @RequestMapping("/api/messages")
 public class MessageController {
     @Autowired
-    private MessageRepository messageRepository;
+    private MessageService messageService;
 
     @GetMapping("/{senderId}/{receiverId}")
-    public List<Message> getMessages(@PathVariable int senderId, @PathVariable int receiverId) {
-        return messageRepository.findBySenderIdAndReceiverIdOrReceiverIdAndSenderIdOrderByTimestampAsc(
-                senderId, receiverId, senderId, receiverId);
+    public List<GetMessageDTO> getBySenderIdAndProviderId(@PathVariable int senderId, @PathVariable int receiverId) {
+        List<GetMessageDTO> messages = messageService.findBySenderIdAndReceiverId(senderId,receiverId);
+        return new ResponseEntity<>(messages, HttpStatus.OK).getBody();
     }
 }
