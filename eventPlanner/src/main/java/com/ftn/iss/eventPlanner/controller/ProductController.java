fbb5eaa8985da0e8bf91fa870743a557b40ffc03
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-13 14:05:50 +0100

    Add authorization to create product endpoint

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/ProductController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -60,7 +61,7 @@ public class ProductController {
         return new ResponseEntity<>(product, HttpStatus.OK);
     }
 
-
+    @PreAuthorize("hasAnyAuthority('PROVIDER')")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CreatedProductDTO> createProduct(@RequestBody CreateProductDTO product) throws Exception {
         CreatedProductDTO createdProduct = productService.create(product);
