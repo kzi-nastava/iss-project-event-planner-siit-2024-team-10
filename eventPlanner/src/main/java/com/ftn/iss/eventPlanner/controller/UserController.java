d40fff59979bf607a2973b659124952a59b9f7dc
Mijat Krivokapić <mijat2004@gmail.com>
2025-01-17 19:10:45 +0100

    Add update user endpoint

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/UserController.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/controller/UserController.java
@@ -3,15 +3,19 @@ package com.ftn.iss.eventPlanner.controller;
 import com.ftn.iss.eventPlanner.dto.*;
 import com.ftn.iss.eventPlanner.dto.company.CreatedCompanyDTO;
 import com.ftn.iss.eventPlanner.dto.company.GetCompanyDTO;
+import com.ftn.iss.eventPlanner.dto.company.UpdateCompanyDTO;
 import com.ftn.iss.eventPlanner.dto.company.UpdatedCompanyDTO;
 import com.ftn.iss.eventPlanner.dto.location.GetLocationDTO;
 import com.ftn.iss.eventPlanner.dto.user.*;
 import com.ftn.iss.eventPlanner.model.*;
 import com.ftn.iss.eventPlanner.services.UserService;
+import jakarta.validation.Valid;
+import org.hibernate.annotations.CollectionTypeRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,27 +24,12 @@ import java.util.Collection;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<CreatedUserDTO> createUser(@RequestBody CreateUserDTO user){
-        CreatedUserDTO savedUser = new CreatedUserDTO();
-        savedUser.setId(1);
-        savedUser.setRole(user.getRole());
-        savedUser.setEmail(user.getEmail());
-        savedUser.setFirstName(user.getFirstName());
-        savedUser.setLastName(user.getLastName());
-        savedUser.setPhoneNumber(user.getPhoneNumber());
-        savedUser.setProfilePhoto(user.getProfilePhoto());
-        if(savedUser.getRole()==Role.PROVIDER){
-            savedUser.setCompany(new CreatedCompanyDTO(1,user.getCompany().getEmail(),user.getCompany().getName(),user.getCompany().getPhoneNumber(),user.getCompany().getDescription(),user.getCompany().getPhotos(), null));
-        }
-
-        return new ResponseEntity<CreatedUserDTO>(savedUser, HttpStatus.CREATED);
-    }
 
     @GetMapping(value = "/{accountId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GetUserDTO> getUserDetails(@PathVariable("accountId") int accountId) {
@@ -48,24 +37,12 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PutMapping(value = "/{id}/activate")
-    public ResponseEntity<?> activateUser(@PathVariable int id) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
 
-    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UpdatedUserDTO> updateUser(@RequestBody UpdateUserDTO user, @PathVariable int id) {
-        UpdatedUserDTO updatedUser = new UpdatedUserDTO();
-        updatedUser.setId(id);
-        updatedUser.setFirstName(user.getFirstName());
-        updatedUser.setLastName(user.getLastName());
-        updatedUser.setPhoneNumber(user.getPhoneNumber());
-        updatedUser.setProfilePhoto(user.getProfilePhoto());
-        if(user.getCompany()!=null){
-            updatedUser.setCompany(new UpdatedCompanyDTO(user.getCompany().getId(),user.getCompany().getPhoneNumber(),user.getCompany().getDescription(),user.getCompany().getPhotos(), null));
-        }
-
-        return new ResponseEntity<UpdatedUserDTO>(updatedUser, HttpStatus.OK);
+    @PreAuthorize("hasAnyAuthority('EVENT_ORGANIZER','PROVIDER')")
+    @PutMapping(value = "/{accountId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UpdatedUserDTO> updateUser(@Valid @RequestBody UpdateUserDTO user, @PathVariable int accountId) {
+        UpdatedUserDTO updatedUserDTO = userService.updateUser(accountId, user);
+        return new ResponseEntity<UpdatedUserDTO>(updatedUserDTO, HttpStatus.OK);
     }
 
     @PutMapping(value = "/{id}/changePassword", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
