ea2a135fd7188187067a812e27fb600b7f19ded0
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-01-31 13:50:08 +0100

    Update notification model

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Notification.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/model/Notification.java
@@ -5,19 +5,27 @@ import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Entity
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    @Column(nullable=false)
-    private boolean isRead;
+    @Column(nullable = false)
+    private String title;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime date;
+
+    @Column(nullable = false)
+    private boolean isRead;
 
     public Notification() {
     }
