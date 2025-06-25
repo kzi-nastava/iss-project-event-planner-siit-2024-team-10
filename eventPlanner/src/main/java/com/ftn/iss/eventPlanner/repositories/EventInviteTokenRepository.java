4f4ee8caf4e7c5a32ab7d75079eda342c7e5a97d
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-06-25 12:13:16 +0200

    Remove unused function in invite token repository

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/EventInviteTokenRepository.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/EventInviteTokenRepository.java
@@ -1,6 +1,5 @@
 package com.ftn.iss.eventPlanner.repositories;
 
-import com.ftn.iss.eventPlanner.model.Event;
 import com.ftn.iss.eventPlanner.model.EventInviteToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EventInviteTokenRepository extends JpaRepository<EventInviteToken, Integer> {
     EventInviteToken findByToken(String token);
-    boolean existsByEmailAndEvent(String email, Event event);
 }
