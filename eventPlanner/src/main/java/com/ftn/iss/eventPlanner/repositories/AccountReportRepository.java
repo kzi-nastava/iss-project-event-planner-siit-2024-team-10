a04835f66fe82ea82cfb1810b745b059cc669180
Marina Ivanovic <ivanovic.sv6.2022@uns.ac.rs>
2025-07-17 18:48:42 +0200

    Update account report repo

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/AccountReportRepository.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/repositories/AccountReportRepository.java
@@ -1,7 +1,11 @@
 package com.ftn.iss.eventPlanner.repositories;
 
 import com.ftn.iss.eventPlanner.model.AccountReport;
+import com.ftn.iss.eventPlanner.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface AccountReportRepository extends JpaRepository<AccountReport,Integer> {
+    boolean existsByReporter_EmailAndReportee_Email(String reporterEmail, String reporteeEmail);
+    Optional<AccountReport> findTopByReportee_IdAndStatusOrderByProcessingTimestampDesc(int reporteeId, Status status);
 }
