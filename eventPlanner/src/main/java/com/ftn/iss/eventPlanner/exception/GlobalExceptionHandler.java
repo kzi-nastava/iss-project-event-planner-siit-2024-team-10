4dd0ef2852b097862ef2a4446d927e16349e034c
Mijat Krivokapić <mijat2004@gmail.com>
2025-07-24 18:27:27 +0200

    Add dto validation exception handler

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/exception/GlobalExceptionHandler.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/exception/GlobalExceptionHandler.java
@@ -2,9 +2,13 @@ package com.ftn.iss.eventPlanner.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.webjars.NotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -36,5 +40,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AccountSuspendedException.class)
     public ResponseEntity<String> handleSuspended(AccountSuspendedException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
