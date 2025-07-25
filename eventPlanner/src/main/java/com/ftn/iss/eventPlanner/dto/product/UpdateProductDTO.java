5917d38d5cc50b6495038dfe56074a275ab4cd3a
Mijat Krivokapić <mijat2004@gmail.com>
2025-07-25 13:30:53 +0200

    Refactor update product dto

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/product/UpdateProductDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/product/UpdateProductDTO.java
@@ -1,6 +1,10 @@
 package com.ftn.iss.eventPlanner.dto.product;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +13,24 @@ import java.util.List;
 @Getter
 @Setter
 public class UpdateProductDTO {
+    @NotBlank(message = "Name must not be blank")
     private String name;
+
+    @NotBlank(message = "Description must not be blank")
     private String description;
+
+    @Positive(message = "Price must be greater than 0")
     private double price;
+
+    @Min(value = 0, message = "Discount cannot be negative")
+    @Max(value = 100, message = "Discount cannot be more than 100")
     private double discount;
+
     private List<String> photos;
+
     @JsonProperty("isVisible")
     private boolean isVisible;
+
     @JsonProperty("isAvailable")
     private boolean isAvailable;
 
