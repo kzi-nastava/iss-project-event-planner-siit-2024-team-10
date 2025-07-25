59932ab90951bf79734f5dfee2e682c220ce2e13
Mijat Krivokapić <mijat2004@gmail.com>
2025-07-25 13:32:26 +0200

    Refactor create user dto

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/user/CreateUserDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/user/CreateUserDTO.java
@@ -3,20 +3,43 @@ package com.ftn.iss.eventPlanner.dto.user;
 import com.ftn.iss.eventPlanner.dto.company.CreateCompanyDTO;
 import com.ftn.iss.eventPlanner.dto.location.CreateLocationDTO;
 import com.ftn.iss.eventPlanner.model.Role;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateUserDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+
+    @NotNull(message = "Role is required")
     private Role role;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
+
     private String profilePhoto;
+
+    @Valid
     private CreateLocationDTO location;
+
+    @Valid
     private CreateCompanyDTO company;
 
     public CreateUserDTO() {}
