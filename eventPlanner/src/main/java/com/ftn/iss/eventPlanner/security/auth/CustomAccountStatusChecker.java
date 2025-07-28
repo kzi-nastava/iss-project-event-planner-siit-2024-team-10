9e28403530f9b1e37563a0c27d62b24228767e22
milicaradicc <milica.t.radic@gmail.com>
2025-07-28 18:59:39 +0200

    Refactor

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/security/auth/CustomAccountStatusChecker.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/security/auth/CustomAccountStatusChecker.java
@@ -1,6 +1,7 @@
 package com.ftn.iss.eventPlanner.security.auth;
 
 import com.ftn.iss.eventPlanner.model.Account;
+import com.ftn.iss.eventPlanner.model.AccountStatus;
 import com.ftn.iss.eventPlanner.services.AccountReportService;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,7 +21,7 @@ public class CustomAccountStatusChecker implements UserDetailsChecker {
     public void check(UserDetails user) {
         if (!(user instanceof Account account)) return;
         accountReportService.checkSuspensionStatus(account.getId());
-        if (!account.isEnabled()) {
+        if (!account.isEnabled() && account.getStatus()!= AccountStatus.SUSPENDED) {
             throw new DisabledException("Account is not enabled.");
         }
     }
