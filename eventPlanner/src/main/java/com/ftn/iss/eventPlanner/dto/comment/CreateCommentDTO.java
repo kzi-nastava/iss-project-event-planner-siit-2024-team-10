538d517d70b00a4b5a2611b59c6cad42da4baa30
milicaradicc <milica.t.radic@gmail.com>
2025-07-29 23:08:14 +0200

    Update comment dtos

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/comment/CreateCommentDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/comment/CreateCommentDTO.java
@@ -1,13 +1,20 @@
 package com.ftn.iss.eventPlanner.dto.comment;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateCommentDTO {
+    @NotNull(message = "Content is required")
     private String content;
+    @Valid
+    @NotNull
     private int account;
+    @NotNull(message = "Rating is required")
+
     private int rating;
     public CreateCommentDTO(){
 
