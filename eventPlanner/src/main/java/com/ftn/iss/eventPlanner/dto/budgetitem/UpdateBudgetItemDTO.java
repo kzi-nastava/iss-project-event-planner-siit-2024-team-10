9146b068474242373e5656c8f87020da05b49da0
milicaradicc <milica.t.radic@gmail.com>
2025-07-29 23:00:24 +0200

    Update budget item dtos

--- eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/budgetitem/UpdateBudgetItemDTO.java
+++ eventPlanner/src/main/java/com/ftn/iss/eventPlanner/dto/budgetitem/UpdateBudgetItemDTO.java
@@ -1,11 +1,13 @@
 package com.ftn.iss.eventPlanner.dto.budgetitem;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateBudgetItemDTO {
+    @NotNull
     private int amount;
 
 }
