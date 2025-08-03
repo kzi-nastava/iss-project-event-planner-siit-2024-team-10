db011c96102fd0c2b626a0a80078fef4d6420c09
Mijat Krivokapić <mijat2004@gmail.com>
2025-08-04 00:20:37 +0200

    Implement navigation bar pom

---
+++ eventPlanner/src/test/java/com/ftn/iss/eventPlanner/selenium/page/NavigationBarPage.java
@@ -0,0 +1,50 @@
+package com.ftn.iss.eventPlanner.selenium.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class NavigationBarPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public NavigationBarPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//button[.//mat-icon[text()='menu']]")
+    private WebElement menuButton;
+
+    @FindBy(xpath = "//button[.//mat-icon[text()='account_circle']]")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//button[.//span[text()='Create event']]")
+    private WebElement createEventMenuItem;
+
+    public void openMenuAndClickCreateEvent() {
+        // Click the menu icon
+        wait.until(ExpectedConditions.elementToBeClickable(menuButton)).click();
+
+        // Wait for the "Create event" menu item to appear
+        wait.until(ExpectedConditions.visibilityOf(createEventMenuItem));
+
+        // Click on "Create event"
+        createEventMenuItem.click();
+    }
+
+    public void openLoginPage() {
+        // Click the login button
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+
+        // Wait for the login page to load
+        wait.until(ExpectedConditions.urlContains("/login"));
+    }
+}
