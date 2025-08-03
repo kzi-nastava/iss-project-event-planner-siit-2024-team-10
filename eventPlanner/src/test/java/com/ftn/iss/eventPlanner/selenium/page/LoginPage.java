3dfc113d1def772b0891d3621bfc2b356a2640cc
Mijat Krivokapić <mijat2004@gmail.com>
2025-08-03 18:18:35 +0200

    Implement login pom

---
+++ eventPlanner/src/test/java/com/ftn/iss/eventPlanner/selenium/page/LoginPage.java
@@ -0,0 +1,41 @@
+package com.ftn.iss.eventPlanner.selenium.page;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class LoginPage {
+    private WebDriver driver;
+    private static final String PAGE_URL = "http://localhost:4200/login";
+
+    @FindBy(css = "input[formcontrolname='email']")
+    private WebElement emailInput;
+
+    @FindBy(css = "input[formcontrolname='password']")
+    private WebElement passwordInput;
+
+    @FindBy(css = "button.login-button")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        driver.get(PAGE_URL);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String email, String password) {
+        emailInput.clear();
+        emailInput.sendKeys(email);
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+}
