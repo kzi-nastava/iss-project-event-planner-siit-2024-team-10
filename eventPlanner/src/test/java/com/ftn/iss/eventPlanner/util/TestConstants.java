40422a049bfead5da206fd6e60401e6c73487cee
milicaradicc <milica.t.radic@gmail.com>
2025-08-06 17:20:14 +0200

    Update controller tests

---
+++ eventPlanner/src/test/java/com/ftn/iss/eventPlanner/util/TestConstants.java
@@ -0,0 +1,28 @@
+package com.ftn.iss.eventPlanner.util;
+
+public class TestConstants {
+    public static final String BASE = "/api/events";
+    public static final int EXISTING_EVENT_ID = 1;
+    public static final int NON_EXISTENT_EVENT_ID = 999;
+    public static final int EXISTING_BUDGET_ITEM_ID = 1;
+    public static final int NON_EXISTENT_BUDGET_ITEM_ID = 999;
+    public static final int EXISTING_OFFERING_ID = 1;
+    public static final int NON_EXISTENT_OFFERING_ID = 999;
+    public static final int DELETED_EVENT_ID = 6;
+    public static final int DELETED_OFFERING_ID = 10;
+    public static final int EXPENSIVE_OFFERING_ID = 3;
+    public static final int ALREADY_PURCHASED_PRODUCT_ID = 2;
+    public static final int DELETED_BUDGET_ITEM_ID = 2;
+    public static final int DELETED_CATEGORY_ID = 3;
+    public static final int EVENT_WITH_BUDGET_ITEM = 3;
+    public static final int BUDGET_ITEM_TO_DELETE = 3;
+    public static final int NON_EXISTENT_ORGANIZER_ID = 999;
+    public static final int UPDATABLE_EVENT_ID = 3;
+    public static final int NON_EXISTENT_EVENT_TYPE_ID = 999;
+    public static final int DELETABLE_EVENT_ID = 4;
+    public static final int EXISTING_AGENDA_ITEM_ID = 1;
+    public static final int NON_EXISTENT_AGENDA_ITEM_ID = 999;
+    public static final int EVENT_WITHOUT_BUDGET_ITEM = 2;
+    public static final int EVENT_WITHOUT_BUDGET = 4;
+    public static final int BUDGET_ITEM_WITH_SERVICES_AND_PRODUCTS = 4;
+}
